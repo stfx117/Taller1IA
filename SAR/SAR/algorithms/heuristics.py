038a54dd11f358a2356b2a66ac4725ff988954e5
stfx117 <jd.casallasf1@uniandes.edu.co>
2026-02-20 10:49:15 -0500

    funcion heuristica terminada

--- SAR/SAR/algorithms/heuristics.py
+++ SAR/SAR/algorithms/heuristics.py
@@ -42,5 +42,53 @@ def survivorHeuristic(state: Tuple[Tuple, Any], problem: MultiSurvivorProblem):
     - Consider: distance to nearest survivor + MST of remaining survivors
     - Balance heuristic strength vs. computation time (do experiments!)
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    if problem.isGoalState(state):
+        return 0
+    
+    posiscion, survivors_grid = state
+    
+    supervivientes = []
+    for m in range(len(survivors_grid)):
+        for n in range(len(survivors_grid[m])):
+            if survivors_grid[m][n]:
+                supervivientes.append((m, n))
+    
+    menor = float("inf")
+    x1, y1 = posiscion
+    for pos in range(len(supervivientes)):
+        x2, y2 = supervivientes[pos]
+        cordenada_x = abs(x1 - x2)
+        cordenada_y = abs(y1 - y2)
+        distancia_manhattan = cordenada_x + cordenada_y
+        if distancia_manhattan < menor:
+            menor = distancia_manhattan
+
+    if len(supervivientes) == 1:
+        return menor
+
+    visitados = set()
+    mst_costo = 0
+    
+    visitados.add(supervivientes[0])
+    
+    while len(visitados) < len(supervivientes):
+        distancia_corta = float("inf")
+        super_cercano = None
+        
+        for visitado in visitados:
+            for superviviente in supervivientes:
+                if superviviente not in visitados:
+                    x1_1 , y1_2 = visitado
+                    x2_1 , y2_2 = superviviente
+                    cordenada_x = abs(x1_1 - x2_1)
+                    cordenada_y = abs(y1_2 - y2_2)
+                    distancia_manhattan = cordenada_x + cordenada_y
+                    if distancia_manhattan < distancia_corta:
+                        distancia_corta = distancia_manhattan
+                        super_cercano = superviviente  
+                    
+        visitados.add(super_cercano)
+        mst_costo += distancia_corta
+    
+    return menor + mst_costo
+    
